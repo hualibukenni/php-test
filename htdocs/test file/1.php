6bc2bb91a6677803a0bab78eada4f9b7dcd41822
hualibukenni <1828686915@qq.com>
2018-03-13 10:14:32 +0800

    第一次传送test file到github上（20180313）；

---
+++ htdocs/test file/1.php
@@ -0,0 +1,7 @@
+<?php
+  $boo = true;
+  if($boo == true)
+    echo"变量$boo为真！";
+  else
+    echo"变量$boo为假!";
+?>
---
+++ htdocs/test file/5.php
@@ -0,0 +1,32 @@
+<?php
+// $username='lhy';
+// $str=<<<"EOD"
+//   <h1>this is a test </h1>
+//   <p>用户名username:{$username}</p>
+// EOD;
+// echo $str;
+// $arr=array(12,'king',true,123.45);
+// var_dump($arr);
+// echo '<hr />';
+// $obj=new StdClass();
+// var_dump($obj);
+// var_dump($s);
+// echo 1+'23aa34a','<br/>';
+// echo 1+true,'<br/>';
+// echo 1+'lhy','<br/>';
+// echo 1+'2e4b5f';
+// $str="A3eb6";
+// echo floatval($str);
+echo PHP_VERSION,'<br />';
+echo PHP_OS,'<br />';
+echo PHP_INT_MAX;
+echo '<br/>';
+define('TEST','this is a test');
+echo TEST,'<hr/>';
+define('TEST_ARRAY',array('a','b','c'));
+var_dump(TEST_ARRAY);
+const WEB_TITLE='BLOG';
+echo '<br/>',constant('TEST'),'<hr/>';
+print_r(get_defined_constants());
+// var_dump('$cc');
+?>
