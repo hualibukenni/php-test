99cca785eb25aab33fcd690b3861ed0103b74942
hualibukenni <1828686915@qq.com>
2018-03-14 10:54:38 +0800

    20180314新增的文件；

---
+++ htdocs/test file/doReg.php
@@ -0,0 +1,4 @@
+<?php
+header('content-type:text/html;charset=utf-8');
+echo 'this is a test';
+echo '用户名：',$_POST['username'],'<br/>';
---
+++ htdocs/test file/doReg1.php
@@ -0,0 +1,4 @@
+<?php
+header('content-type:text/html;charset:utf8');
+echo 'this is code:'.$_POST['pass'];
+ ?>
